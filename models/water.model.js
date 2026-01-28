4f7d57b2d004a22a6116267f278339007b024eca
Emir <emircan.uregen@std.medipol.edu.tr>
2026-01-28 22:50:38 +0300

    Push for water part

---
+++ models/water.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const waterSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Water', waterSchema);
